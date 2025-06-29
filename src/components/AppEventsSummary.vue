6954df92d73f657f286eb9d901dd6575b7efdf53
Mehdi <mehdi.mhiri@gmail.com>
2025-06-29 19:35:18 +0900

    feat: events summary items separate component

--- src/components/AppEventsSummary.vue
+++ src/components/AppEventsSummary.vue
@@ -1,38 +1,11 @@
 <script setup lang="ts">
-import { Icon } from "@iconify/vue";
 import { days, WEEK_START, WEEK_END } from "@/util";
 import { useEvents } from "@/compasables/useEvents";
+import AppEventsSummaryItem from "./AppEventsSummaryItem.vue";
 
 const { events } = defineProps<{
   events: ReturnType<typeof useEvents>;
 }>();
-
-function getKey(day: number, slot: number) {
-  return `${day}-${slot}`;
-}
-
-function getActivity(day: number, slot: number) {
-  const key = getKey(day, slot);
-  return events.data.value.find(
-    ({ day: d, start }) => getKey(d, start) === key,
-  );
-}
-
-function getActivityLength(day: number, slot: number): number {
-  const activitySlot = getActivity(day, slot);
-  if (!activitySlot) {
-    return 0;
-  }
-
-  if (activitySlot.day === -1) {
-    return 1;
-  }
-
-  const start = activitySlot.start;
-  const end = activitySlot.end;
-
-  return (end - start) * 2 + 1;
-}
 </script>
 
 <template>
@@ -48,49 +21,7 @@ function getActivityLength(day: number, slot: number): number {
         },
       ]"
     >
-      <Icon icon="carbon:time-filled" />
-      <div>
-        {{
-          events.data.value
-            .filter((a) => a.day === day)
-            .reduce(
-              (acc, cur) => acc + getActivityLength(cur.day, cur.start) / 2,
-              0,
-            )
-        }}h
-      </div>
-      <div
-        v-if="events.data.value.filter((a) => a.day === day).length > 0"
-        class="group-hover/summary:opacity-100 group-hover/summary:z-10 opacity-0 transition-opacity absolute bottom-full flex flex-col justify-center items-end bg-slate-950 rounded-sm mb-1 text-xs px-4 py-2 gap-1"
-        v-html="
-          Object.entries(
-            events.data.value
-              .filter((a) => a.day === day)
-              .reduce(
-                (acc, cur) => {
-                  cur.note.split('\n').forEach((line) => {
-                    const matches = line.match(/#(\w+)/g);
-                    if (matches) {
-                      matches.forEach((match) => {
-                        const tag = match.slice(1);
-                        acc[tag] =
-                          (acc[tag] ?? 0) +
-                          getActivityLength(cur.day, cur.start) / 2;
-                      });
-                    }
-                  });
-                  return acc;
-                },
-                {} as Record<string, number>,
-              ),
-          )
-            .map(([tag, value]) => {
-              return `<div class='flex flex-row gap-1'><span class='bg-sky-300 text-sky-800 font-bold rounded-sm
-px-1'>${tag}</span> ${value.toFixed(1)}h</div>`;
-            })
-            .join('<br>')
-        "
-      ></div>
+      <AppEventsSummaryItem :events="events.data" :day="day" />
     </div>
   </div>
 </template>
