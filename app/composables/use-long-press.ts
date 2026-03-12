0712f552eecb8f0518b244919253a92f7c61a52c
Mehdi <mehdi.mhiri@gmail.com>
2026-03-12 21:46:55 +0100

    feat(long-press): add mouse distance check

--- app/composables/use-long-press.ts
+++ app/composables/use-long-press.ts
@@ -7,6 +7,8 @@ interface UseLongPressProps {
   onRelease: (e: MouseEvent) => void;
 }
 
+const DISTANCE_THRESHOLD = 10;
+
 export default function useLongPress({
   target,
   onLongPress,
@@ -15,11 +17,23 @@ export default function useLongPress({
   const longPressTimeout = shallowRef(0);
   const isLongPress = shallowRef(false);
 
+  const { x, y } = useMouse();
+  const initialX = shallowRef(0);
+  const initialY = shallowRef(0);
+
+  function getDistanceFromInitial() {
+    return Math.sqrt(
+      Math.pow(x.value - initialX.value, 2) +
+        Math.pow(y.value - initialY.value, 2),
+    );
+  }
+
   function onMouseUp(e: MouseEvent) {
     clearTimeout(longPressTimeout.value);
     if (!isLongPress.value) {
       return;
     }
+
     isLongPress.value = false;
     onRelease(e);
   }
@@ -30,7 +44,14 @@ export default function useLongPress({
       return;
     }
 
+    initialX.value = x.value;
+    initialY.value = y.value;
+
     longPressTimeout.value = setTimeout(() => {
+      if (getDistanceFromInitial() > DISTANCE_THRESHOLD) {
+        return;
+      }
+
       isLongPress.value = true;
       onLongPress(e);
     }, 500);
