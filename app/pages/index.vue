c9df0067333735e8a714fa99ee5360ed3f5d29bf
Mehdi <mehdi.mhiri@gmail.com>
2026-03-13 21:30:14 +0100

    fix: remove client only for now

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -39,53 +39,51 @@ useMousePressed({
 </script>
 
 <template>
-  <ClientOnly>
-    <div class="flex h-screen w-screen flex-col gap-1">
-      <Header />
-      <div class="flex h-full w-full gap-1 px-1">
-        <div class="grid h-full grid-rows-12">
-          <TimeIndicator />
-        </div>
-        <div ref="week" class="grid h-full w-full grid-cols-7 gap-1">
+  <div class="flex h-screen w-screen flex-col gap-1">
+    <Header />
+    <div class="flex h-full w-full gap-1 px-1">
+      <div class="grid h-full grid-rows-12">
+        <TimeIndicator />
+      </div>
+      <div ref="week" class="grid h-full w-full grid-cols-7 gap-1">
+        <div
+          v-for="day in days"
+          :key="day"
+          class="relative grid h-full rounded-sm"
+        >
+          <CurrentTimeIndicator :day="day" :day-height="weekHeight" />
+          <DaySlot
+            v-for="time in availableSlots"
+            :id="`slot-${day}-${time.index}`"
+            :key="`${day}-${time.index}`"
+            :day="day"
+            :time="time"
+            :current-event="currentEvent"
+            @slot-hover="onSlotHover"
+          />
           <div
-            v-for="day in days"
-            :key="day"
-            class="relative grid h-full rounded-sm"
+            class="pointer-events-none absolute grid h-full w-full grid-rows-24 gap-1"
           >
-            <CurrentTimeIndicator :day="day" :day-height="weekHeight" />
-            <DaySlot
-              v-for="time in availableSlots"
-              :id="`slot-${day}-${time.index}`"
-              :key="`${day}-${time.index}`"
-              :day="day"
-              :time="time"
-              :current-event="currentEvent"
-              @slot-hover="onSlotHover"
+            <Event
+              v-for="event in events.filter((e) => e.day === day)"
+              :key="`${day}-${event.id}`"
+              :event="event"
             />
-            <div
-              class="pointer-events-none absolute grid h-full w-full grid-rows-24 gap-1"
-            >
-              <Event
-                v-for="event in events.filter((e) => e.day === day)"
-                :key="`${day}-${event.id}`"
-                :event="event"
-              />
-            </div>
           </div>
         </div>
       </div>
-      <div class="flex min-h-8 w-full gap-1 px-1 pb-1">
-        <div class="-translate-y-4 text-xs">20:00</div>
-        <div class="grid w-full grid-cols-7 gap-1">
-          <DayProgress
-            v-for="day in days"
-            :key="`${day}-progress`"
-            :day="day"
-            :events="events"
-          />
-        </div>
+    </div>
+    <div class="flex min-h-8 w-full gap-1 px-1 pb-1">
+      <div class="-translate-y-4 text-xs">20:00</div>
+      <div class="grid w-full grid-cols-7 gap-1">
+        <DayProgress
+          v-for="day in days"
+          :key="`${day}-progress`"
+          :day="day"
+          :events="events"
+        />
       </div>
     </div>
     <EventModal />
-  </ClientOnly>
+  </div>
 </template>
