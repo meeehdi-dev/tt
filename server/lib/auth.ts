17352caec530359b4b02c69643f10d8265565e2c
Mehdi <mehdi.mhiri@gmail.com>
2026-03-14 21:49:18 +0100

    chore: rename import

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -2,7 +2,7 @@ import { v7 as uuidv7 } from "uuid";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { getDb } from "~~/server/db";
-import { account, session, user, verification } from "../db/schema/auth";
+import { account, session, user, verification } from "~~/server/db/schema/auth";
 
 const db = getDb();
 
